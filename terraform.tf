f0e4f5a294ca24401162653823d14ffef161ad79
Yurii Zakharov <yurii_zakharov@epam.com>
2024-01-31 12:09:10 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "Yurii_Zakharov_Org"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
